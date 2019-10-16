57620a8be31e260b566cb3264f85c4e7d549200a
Kiana Mills <kmills@ox-u4.21-school.ru>
2019-10-16 19:36:48 +0300

    still

--- D06/ex02/Vector.class.php
+++ D06/ex02/Vector.class.php
@@ -3,49 +3,30 @@ require_once '../ex01/Vertex.class.php';
 
 class Vector
 {
-	public	static $verbose = FALSE;
-	private	$x0;
-	private	$y0;
-	private	$z0;
-	private	$x1;
-	private	$y1;
-	private	$z1;
-	private	$x;
-	private	$y;
-	private	$z;
-	private	$w;
-	private	$orig;
-	private	$dest;
+	private $x;
+	private $y;
+	private $z;
+	private $w = 0;
+	static $verbose = false;
 
 	public function getX() { return $this->x; }
 	public function getY() { return $this->y; }
 	public function getZ() { return $this->z; }
 	public function getW() { return $this->w; }
-	public static function doc()
-	{
-		return file_get_contents("./Vector.doc.txt");
-	}
 
-	public function __Construct(array $origdest)
+	public function __construct($array)
 	{
-		if (!isset($origdest['orig']))
-			$this->orig = new Vertex();
-		else
-			$this->orig = $origdest['orig'];
-		$this->x0 = $this->orig->getX();
-		$this->y0 = $this->orig->getY();
-		$this->z0 = $this->orig->getZ();
-		if (!isset($origdest['dest']))
-			$this->dest = new Vertex();
-		else
-			$this->dest = $origdest['dest'];
-		$this->x1 = $this->dest->getX();
-		$this->y1 = $this->dest->getY();
-		$this->z1 = $this->dest->getZ();
-		$this->x = $this->x1 - $this->x0;
-		$this->y = $this->y1 - $this->y0;
-		$this->z = $this->z1 - $this->z0;
-		$this->w = 0.0;
+		if (isset($array['dest']) && $array['dest'] instanceof Vertex)
+		{
+			if (isset($array['orig']) && $array['orig'] instanceof Vertex)
+				$orig = new Vertex(array('x' => $array['orig']->getX(), 'y' => $array['orig']->getY(), 'z' => $array['orig']->getZ()));
+			else
+				$orig = new Vertex(array('x' => 0, 'y' => 0, 'z' => 0));
+			$this->x = $array['dest']->getX() - $orig->getX();
+			$this->y = $array['dest']->getY() - $orig->getY();
+			$this->z = $array['dest']->getZ() - $orig->getZ();
+			$this->w = 0;
+		}
 		if (self::$verbose == TRUE)
 			echo $this.' constructed'.PHP_EOL;
 		return ;
@@ -60,93 +41,71 @@ class Vector
 
 	function __toString()
 	{
-		return (sprintf("Vector( x:%0.2f, y:%0.2f, z:%0.2f, w:%0.2f )", array($this->_x, $this->_y, $this->_z, $this->_w)));
+		return (sprintf("Vector( x:%0.2f, y:%0.2f, z:%0.2f, w:%0.2f )",$this->x, $this->y, $this->z, $this->w));
+	}
+
+	public static function doc()
+	{
+		return file_get_contents("./Vector.doc.txt");
 	}
 
 	public function magnitude()
 	{
-		return (sqrt($this->x**2 + $this->y**2 + $this->z**2));
+		return (float)sqrt(($this->x * $this->x) + ($this->y * $this->y) + ($this->z * $this->z));
 	}
 
 	public function normalize()
 	{
 		$l = $this->magnitude();
-		if ($l != 0)
-		{
-			$xx = $this->x/$l;
-			$yy = $this->y/$l;
-			$zz = $this->z/$l;
-			$qwe = new Vector(array('dest'=>new Vertex(array('x' => $xx, 'y' => $yy, 'z' => $zz))));
-		}
-		return $qwe;
+		if ($l == 1)
+			return clone $this;
+		return new Vector(array('dest' => new Vertex(array
+		('x' => $this->x / $l, 'y' => $this->y / $l, 'z' => $this->z / $l))));
 	}
 
 	public function add(Vector $rhs)
 	{
-		$xx = $this->x + $rhs->x;
-		$yy = $this->y + $rhs->y;
-		$zz = $this->z + $rhs->z;
-		$qwe = new Vector(array('dest'=>new Vertex(array('x' => $xx, 'y' => $yy, 'z' => $zz))));
-		return $qwe;
+		return new Vector(array('dest' => new Vertex(array('x' => $this->x + $rhs->x, 
+		'y' => $this->y + $rhs->y, 'z' => $this->z + $rhs->z))));
 	}
 
 	public function sub(Vector $rhs)
 	{
-		$xx = $this->x - $rhs->x;
-		$yy = $this->y - $rhs->y;
-		$zz = $this->z - $rhs->z;
-		$qwe = new Vector(array('dest'=>new Vertex(array('x' => $xx, 'y' => $yy, 'z' => $zz))));
-		return $qwe;
+		return new Vector(array('dest' => new Vertex(array('x' => $this->x - $rhs->x, 
+		'y' => $this->y - $rhs->y, 'z' => $this->z - $rhs->z))));
 	}
 
 	public function opposite()
 	{
-		$xx = - $this->x;
-		$yy = - $this->y;
-		$zz = - $this->z;
-		$qwe = new Vector(array('dest'=>new Vertex(array('x' => $xx, 'y' => $yy, 'z' => $zz))));
-		return $qwe;
+		return new Vector(array('dest' => new Vertex
+		(array('x' => $this->x * -1, 'y' => $this->y * -1, 'z' => $this->z * -1))));
 	}
 
 	public function scalarProduct($k)
 	{
-		$xx = $this->x * $k;
-		$yy = $this->y * $k;
-		$zz = $this->z * $k;
-		$qwe = new Vector(array('dest'=>new Vertex(array('x' => $xx, 'y' => $yy, 'z' => $zz))));
-		return $qwe;
+		return new Vector(array('dest' => new Vertex
+		(array('x' => $this->x * $k, 'y' => $this->y * $k, 'z' => $this->z * $k))));
+	}
+
+	public function dotProduct(Vector $rhs)
+	{
+		return (float)(($this->x * $rhs->x) + ($this->y * $rhs->y) + ($this->z * $rhs->z));
 	}
 
 	public function crossProduct(Vector $rhs)
 	{
-		$xx = $this->y*$rhs->z - $this->z*$rhs->y;
-		$yy = $this->z*$rhs->x - $this->x*$rhs->z;
-		$zz = $this->x*$rhs->y - $this->y*$rhs->x;
-		$qwe = new Vector(array('dest'=>new Vertex(array('x' => $xx, 'y' => $yy, 'z' => $zz))));
-		return $qwe;
-	}
-
-	public function dotProduc(Vector $rhs)
-	{
-		return ($this->x * $rhs->x + $this->y * $rhs->y + $this->z * $rhs->z);
+		return new Vector(array('dest' => new Vertex(array(
+			'x' => $this->y * $rhs->getZ() - $this->z * $rhs->getY(),
+			'y' => $this->z * $rhs->getX() - $this->x * $rhs->getZ(),
+			'z' => $this->x * $rhs->getY() - $this->y * $rhs->getX()
+		))));
 	}
 
 	public function cos(Vector $rhs)
 	{
-		return ($this->dotProduc($rhs) / ($this->magnitude() * $rhs->magnitude()));
+		return ($this->dotProduct($rhs) / sqrt
+		((($this->x * $this->x) + ($this->y * $this->y) + ($this->z * $this->z)) * 
+		(($rhs->x * $rhs->x) + ($rhs->y * $rhs->y) + ($rhs->z * $rhs->z))));
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
 ?>
